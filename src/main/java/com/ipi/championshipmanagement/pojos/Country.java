3bac022fa2ba3c0ff92d0824ce37db4234982560
CamilleMMi <camillemichaudmeli@gmail.com>
2024-03-05 16:46:24 +0100

    Country's Entity & Implementation

--- src/main/java/com/ipi/championshipmanagement/pojos/Country.java
+++ src/main/java/com/ipi/championshipmanagement/pojos/Country.java
@@ -1,4 +1,62 @@
 package com.ipi.championshipmanagement.pojos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class Country {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+    private String logo;
+
+    @OneToMany(mappedBy = "country")
+    private List<Championship> championships;
+
+    public Country() {
+    }
+
+    public Country(String name, String logo) {
+        this.name = name;
+        this.logo = logo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public List<Championship> getChampionships() {
+        return championships;
+    }
+
+    public void setChampionships(List<Championship> championships) {
+        this.championships = championships;
+    }
 }
