c7cbf4913a8133117e41fe3891fd3a9451690d13
CamilleMMi <camillemichaudmeli@gmail.com>
2024-03-05 15:37:11 +0100

    Match's Entity & Implementation

--- src/main/java/com/ipi/championshipmanagement/dao/MatchDao.java
+++ src/main/java/com/ipi/championshipmanagement/dao/MatchDao.java
@@ -1,4 +1,7 @@
 package com.ipi.championshipmanagement.dao;
 
-public interface MatchDao {
+import com.ipi.championshipmanagement.pojos.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchDao extends JpaRepository<Match, Long> {
 }
