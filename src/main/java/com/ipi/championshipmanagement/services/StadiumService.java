71cfe5236f74e6d13e11810621db3186a7168dd1
CamilleMMi <camillemichaudmeli@gmail.com>
2024-03-05 15:50:17 +0100

    Stadium's Entity & Implementation

--- src/main/java/com/ipi/championshipmanagement/services/StadiumService.java
+++ src/main/java/com/ipi/championshipmanagement/services/StadiumService.java
@@ -1,4 +1,16 @@
 package com.ipi.championshipmanagement.services;
 
+import com.ipi.championshipmanagement.pojos.Stadium;
+
+import java.util.List;
+
 public interface StadiumService {
+    List<Stadium> getAllStadiums();
+
+    Stadium getStadiumById(long stadiumId);
+
+    Stadium saveStadium(Stadium stadium);
+
+    void deleteStadium(long stadiumId);
+
 }
