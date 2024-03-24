0d546a6454fdcf6874a2b2bff006be674a8c2c08
Steb <72015326+EstebanLouerat@users.noreply.github.com>
2024-03-24 17:53:39 +0100

    Edit score

--- src/main/java/com/ipi/championshipmanagement/controller/ResultController.java
+++ src/main/java/com/ipi/championshipmanagement/controller/ResultController.java
@@ -7,6 +7,7 @@ import com.ipi.championshipmanagement.services.GameService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @Controller
 public class ResultController {
 
+    Boolean editing = false;
     private final GameService gameService;
     private final ChampionshipService championshipService;
     public ResultController(GameService gameService, ChampionshipService championshipService) {
@@ -28,10 +30,48 @@ public class ResultController {
         Championship currentChampionship = championshipService.findChampionshipById(id);
         List<Game> games = gameService.getGamesByChampionship(currentChampionship);
 
+        model.addAttribute("editing", editing);
         model.addAttribute("games", games);
         model.addAttribute("idChampionship", id);
         model.addAttribute("championships", championships);
         model.addAttribute("currChamp", currentChampionship);
         return "result";
+    }
+
+    @PostMapping({"/result/toggle"})
+    public String toggleEditing(@RequestParam("id") Long id, Model model) {
+        editing = !editing;
+        model.addAttribute("editing", editing);
+        return "redirect:/result?id=" + id;
+    }
+
+    @PostMapping({"/result/save"})
+    public String saveEditing(@RequestParam("goalClub1") int goalClub1,
+                              @RequestParam("goalClub2") int goalClub2,
+                              @RequestParam("gameId") int gameId,
+                              @RequestParam("id") Long id, Model model) {
+        if (goalClub1 < 0 || goalClub2 < 0) {
+            model.addAttribute("errorMessage", "Goal values must be positive numbers.");
+            return "redirect:/result?id=" + id;
+        }
+
+        editing = false;
+
+        Game game = gameService.getGameById(gameId);
+
+        game.setGoalClub1(goalClub1);
+        game.setGoalClub2(goalClub2);
+
+        gameService.save(game);
+
+        model.addAttribute("editing", editing);
+        return "redirect:/result?id=" + id;
+    }
+
+    @PostMapping({"/result/cancel"})
+    public String cancelEditing(@RequestParam("id") Long id, Model model) {
+        editing = false;
+        model.addAttribute("editing", editing);
+        return "redirect:/result?id=" + id;
     }
 }
